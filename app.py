497d804e6fcb52cf4022a9e0bb0c0683db465f0a
Devansh lathiya <devansh.lathiya21@vit.edu>
2023-11-03 02:24:22 +0530

    change the app.py

--- app.py
+++ app.py
@@ -1,34 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import cv2
 import os
 
 app = Flask(__name__)
 
+# Set up a directory to store captured images
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Function to capture and save image
+def capture_image():
+    try:
+        # Check if camera is available
+        cap = cv2.VideoCapture(0)
+        if not cap.isOpened():
+            raise Exception("Could not access the camera")
+
+        # Capture image
+        ret, frame = cap.read()
+        cap.release()
+
+        if ret:
+            # Save image
+            img_path = os.path.join(app.config['UPLOAD_FOLDER'], 'captured_image.jpg')
+            cv2.imwrite(img_path, frame)
+            return img_path
+        else:
+            raise Exception("Failed to capture image")
+    except Exception as e:
+        return str(e)
+
+# Route to render the login page
 @app.route('/')
 def login():
-    return render_template("login.html")
+    return render_template('login.html')
 
-@app.route('/runcaptureimage')
-def run_captureimage():
-    cap = cv2.VideoCapture(0)
-    
-    if cap.isOpened():
-        ret, frame = cap.read()
-        print(ret)
-        print(frame)
-    else:
-        ret = False
-
-    img1 = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-    directory = os.getcwd()
-    os.chdir(directory)
-    print(os.listdir(directory)) 
-    filename = 'Intruder.jpg'
-    cv2.imwrite(os.path.join(directory, filename), img1) 
-
-    cap.release()
-    return "Capture image executed successfully!"
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
